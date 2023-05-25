47e5db6e8216a8e6b3230b46fe4a26eaa2e2c536
feniks1632 <feniks1632@gmail.com>
2023-05-26 00:45:08 +0500

    создал приложение reviews со своей частью кода. создал приложение api со своей частью кодаа

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,21 +1,19 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (
-    CommentsViewSet,
-    ReviewsViewSet,
-)
+# from .views import (CommentsViewSet, ReviewsViewSet)
+
 
 router = routers.DefaultRouter()
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews',
-    ReviewsViewSet,
-    basename='reviews')
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentsViewSet,
-    basename='comments'
-)
+# router.register(
+#     r'titles/(?P<title_id>\d+)/reviews',
+#     ReviewsViewSet,
+#     basename='reviews')
+# router.register(
+#     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+#     CommentsViewSet,
+#     basename='comments'
+# )
 
 
 urlpatterns = [
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,14 +1,15 @@
-from django.db.models import Avg
 from rest_framework import filters, viewsets
 from rest_framework.generics import get_object_or_404
 
-from reviews.models import Comment, Review
+from reviews.models import Review
 from .pagination import CommonPagination
-from .serializers import CommentSerializer, ReviewSerializer  
+from .permissions import IsOwner
+from .serializers import CommentSerializer, ReviewSerializer
+
 
 class CommentsViewset(viewsets.ModelViewSet):
     serializer_class = CommentSerializer()
-  # permission_classes = 
+    permission_classes = (IsOwner,)
     pagination_class = CommonPagination
 
     def get_queryset(self):
@@ -20,20 +21,19 @@ class CommentsViewset(viewsets.ModelViewSet):
         review = get_object_or_404(title.reviews, id=self.kwargs['review_id'])
         serializer.save(author=self.request.user, review=review)
 
+
 class ReviewsViewset(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-  # permission_classes =
+    permission_classes = (IsOwner,)
     filter_backends = (filters.OrderingFilter,)
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         return title.reviews.all()
-    
+
     def perform_create(self, serializer):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         serializer.save(author=self.request.user, title=title)
-
-    def perform_update(self, serializer):
 
 
 class CategoriesViewSet(viewsets.ModelViewSet):
