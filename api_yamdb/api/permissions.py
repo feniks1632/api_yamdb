7dd1d7a32489dbae7973a01755c65c2b0a5a88d6
Александр Ермаков <uraltheoretic@yandex.ru>
2023-06-05 00:58:41 +1200

    ФП 10 исправленный

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,27 +1,32 @@
 from rest_framework import permissions
 
 
-class IsAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_admin
-
-
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated and (
+                    request.user.is_admin)))
+    def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated and (
                     request.user.is_admin)))
 
 
 class IsModeratorOrReadOnly(permissions.BasePermission):
-    def has_permissoin(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated and (
-                    request.user.is_moderator)))
-
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated and (
-                    obj.author == request.user)))
+                or request.user.is_moderator)
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+    
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+    
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
