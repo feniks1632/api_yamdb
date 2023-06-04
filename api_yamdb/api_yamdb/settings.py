d67073c0682ef2660e50fb4a734c2c8aa796df2f
feniks1632 <121809364+feniks1632@users.noreply.github.com>
2023-06-04 12:41:29 +0500

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
 }
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
